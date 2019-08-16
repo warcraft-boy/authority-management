eb3d81763ea6b5205d32c7ea82fc523b22ef474d
chenjianwen <chenjianwen@coralglobal.cn>
2019-08-16 17:59:30 +0800

    添加配置文件

---
+++ authority-api/src/main/java/com/chenjianwen/api/ApiApplication.java
@@ -0,0 +1,9 @@
+package com.chenjianwen.api;
+
+/**
+ * @Description: <br>
+ * @Date: Created in 2019/8/16 <br>
+ * @Author: chenjianwen
+ */
+public class ApiApplication {
+}
---
+++ authority-dal/src/main/java/com/chenjianwen/dal/TestMapper.java
@@ -0,0 +1,9 @@
+package com.chenjianwen.dal;
+
+/**
+ * @Description: <br>
+ * @Date: Created in 2019/8/16 <br>
+ * @Author: chenjianwen
+ */
+public interface TestMapper {
+}
