2f23d419877a8c0437fd812555140821cfbaff37
chenjianwen <chenjianwen@coralglobal.cn>
2019-08-16 13:49:37 +0800

    包添加

---
+++ authority-web/src/main/java/com/chenjianwen/Application.java
@@ -0,0 +1,13 @@
+package com.chenjianwen;
+
+/**
+ * @Description: <br>
+ * @Date: Created in 2019/8/16 <br>
+ * @Author: chenjianwen
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
