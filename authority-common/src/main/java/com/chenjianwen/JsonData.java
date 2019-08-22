7ec5a6f28c9a2098439f67285b8029c0d8373b28
chenjianwen <chenjianwen@coralglobal.cn>
2019-08-22 17:07:06 +0800

    返回响应数据封装

---
+++ authority-common/src/main/java/com/chenjianwen/JsonData.java
@@ -0,0 +1,66 @@
+package com.chenjianwen;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @Description: 返回响应数据
+ * @Date: Created in 2019/8/22 <br>
+ * @Author: chenjianwen
+ */
+@Setter
+@Getter
+public class JsonData<T> {
+
+    /***
+     * 是否返回成功
+     */
+    private Boolean success;
+
+    /**
+     * 返回信息
+     */
+    private String msg;
+
+    /**
+     * 返回数据
+     */
+    private T data;
+
+
+    public JsonData(Boolean success) {
+        this.success = success;
+    }
+
+    public static <T> JsonData<T> success(T t, String msg) {
+        JsonData jsonData = new JsonData(true);
+        jsonData.data = t;
+        jsonData.msg = msg;
+        return jsonData;
+    }
+
+    public static <T> JsonData<T> success(T t) {
+        JsonData jsonData = new JsonData(true);
+        jsonData.data = t;
+        return jsonData;
+    }
+
+    public static <T> JsonData<T> success() {
+        JsonData jsonData = new JsonData(true);
+        return jsonData;
+    }
+
+    public static <T> JsonData<T> fail(T t, String msg) {
+        JsonData jsonData = new JsonData(false);
+        jsonData.data = t;
+        jsonData.msg = msg;
+        return jsonData;
+    }
+
+    public static <T> JsonData<T> fail(String msg) {
+        JsonData jsonData = new JsonData(false);
+        jsonData.msg = msg;
+        return jsonData;
+    }
+
+}
