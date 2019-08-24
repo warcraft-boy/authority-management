6f97cb245fc0df080832572e6cda2b9687029b07
chenjianwen <ChenJevin@163.com>
2019-08-24 12:16:11 +0800

    json工具和上下文

--- authority-web/src/main/java/com/chenjianwen/web/WebApplication.java
+++ authority-web/src/main/java/com/chenjianwen/web/WebApplication.java
@@ -1,8 +1,12 @@
 package com.chenjianwen.web;
 
 import com.chenjianwen.JsonData;
+import com.chenjianwen.api.authority.SysAclModule;
+import com.chenjianwen.dal.authority.SysAclModuleMapper;
 import com.chenjianwen.exceptions.PermissionException;
+import com.chenjianwen.utils.ApplicationContextHelper;
 import com.chenjianwen.utils.BeanValidator;
+import com.chenjianwen.utils.JsonUtils;
 import com.chenjianwen.web.test.TestVo;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections.MapUtils;
@@ -33,6 +37,9 @@ public class WebApplication {
     @ResponseBody
     public JsonData validate(TestVo vo){
         log.info("validate");
+        SysAclModuleMapper sysAclModuleMapper = ApplicationContextHelper.popBean(SysAclModuleMapper.class);
+        SysAclModule sysAclModule = sysAclModuleMapper.selectByPrimaryKey(1L);
+        log.info(JsonUtils.obj2String(sysAclModule));
         BeanValidator.check(vo);
 //        try {
 //            Map<String,String> map = BeanValidator.validateObject(vo);
